5efd69cd74e5c953d86708aab9d20200c70534b2
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-04 20:22:15 +0200

    Update and rename Bonus Score to Conditional Statements/Bonus Score.cs

--- Conditional Statements/Bonus Score.cs
+++ Conditional Statements/Bonus Score.cs
@@ -4,34 +4,35 @@ namespace Bonus_Score
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int number = int.Parse(Console.ReadLine());
+            
             double bonusScore = 0.0;
 
             if (number <= 100)
             {
-                bonusScore=5;         
+                bonusScore = 5;         
             }
-            else if (number <=1000)
+            else if (number <= 1000)
             {
-                bonusScore=number*0.20;
+                bonusScore = number * 0.20;
             }
             else
             {
-                bonusScore=number*0.10;
+                bonusScore = number * 0.10;
             }
-            if (number % 2 ==0)
+            if (number % 2 == 0)
             {
-                bonusScore+=1;
+                bonusScore += 1;
             }
-            else if(number%10==5)
+            else if(number % 10 == 5)
             {
-                bonusScore+=2;
+                bonusScore += 2;
             }
 
             Console.WriteLine(bonusScore);
-            Console.WriteLine(number+bonusScore);
+            Console.WriteLine(number + bonusScore);
         }
     }
 }
