fd0b04d09bd8a7561c3dcaba51faf68508ee6648
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-06 21:15:01 +0200

    Update and rename Charity Campaign to Simple Operations/Charity Campaign.cs

--- Simple Operations/Charity Campaign.cs
+++ Simple Operations/Charity Campaign.cs
@@ -4,19 +4,21 @@ namespace Charity_Campaign
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int days = int.Parse(Console.ReadLine());
             int confectioners = int.Parse(Console.ReadLine());
             int cakes = int.Parse(Console.ReadLine());
             int waffles = int.Parse(Console.ReadLine());
             int pancakes = int.Parse(Console.ReadLine());
+            
             int priceOfCakes = cakes * 45;
             double priceOfWaffles = waffles * 5.80;
             double priceOfPancakes = pancakes * 3.20;
             double totalForDay = (priceOfCakes + priceOfWaffles + priceOfPancakes) * confectioners;
             double totalValue = totalForDay * days;
             double value = totalValue - (totalValue / 8);
+            
             Console.WriteLine($"{value:f2}");
         }
     }
