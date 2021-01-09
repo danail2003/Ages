59c46ad70e6a65a88edf2e99758234b2c1a9bcb6
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-09 19:45:51 +0200

    Update and rename Circle to Simple Operations/Circle.cs

--- Simple Operations/Circle.cs
+++ Simple Operations/Circle.cs
@@ -4,11 +4,13 @@ namespace Circle
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double radius = double.Parse(Console.ReadLine());
+            
             double area = Math.PI * radius * radius;
             double perimeters = 2 * Math.PI * radius;
+            
             Console.WriteLine($"{ area:f2}");
             Console.WriteLine($"{ perimeters:f2}");
         }
