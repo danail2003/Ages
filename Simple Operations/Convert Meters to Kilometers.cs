2909dfc7df2fa51a0e52ad1899c4e26bb7bdeb67
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-11 22:36:41 +0200

    Update and rename Convert Meters to Kilometers to Simple Operations/Convert Meters to Kilometers.cs

--- Simple Operations/Convert Meters to Kilometers.cs
+++ Simple Operations/Convert Meters to Kilometers.cs
@@ -4,10 +4,12 @@ namespace Convert_Meters_to_Kilometers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double meters = double.Parse(Console.ReadLine());
+            
             double kilometers = meters / 1000;
+            
             Console.WriteLine($"{kilometers:f2}");
         }
     }
