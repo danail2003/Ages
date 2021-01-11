d2ecf81bea0601213309d5d72dee21597f7032b8
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-11 22:35:54 +0200

    Update and rename Construction Project to Simple Operations/Construction Project.cs

--- Simple Operations/Construction Project.cs
+++ Simple Operations/Construction Project.cs
@@ -4,11 +4,13 @@ namespace ConstructionProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string architect = Console.ReadLine();
             int projects = int.Parse(Console.ReadLine());
+            
             int hours = 3 * projects;
+            
             Console.WriteLine("The architect {0} will need {1} hours to complete {2} project/s.", architect, hours, projects);
         }
     }
