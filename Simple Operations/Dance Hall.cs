fca9a30fabf9152c1fb4e2beba9ac2379d51fb5d
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-12 21:08:04 +0200

    Update and rename Dance Hall to Simple Operations/Dance Hall.cs

--- Simple Operations/Dance Hall.cs
+++ Simple Operations/Dance Hall.cs
@@ -4,20 +4,22 @@ namespace Dance_Hall
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double l = double.Parse(Console.ReadLine());
             double w = double.Parse(Console.ReadLine());
             double a = double.Parse(Console.ReadLine());
-            double size = (l*100);
+            
+            double size = (l * 100);
             double size1 = (w * 100);
-            double size2 = (a*100);
-            double size3 = (a*100);
+            double size2 = (a * 100);
+            double size3 = (a * 100);
             double sizeOfHall = size * size1;
-            double sizeOfWardrobe = (size2*size3);
+            double sizeOfWardrobe = (size2 * size3);
             double sizeOfBench = (sizeOfHall / 10);
             double freeSpace = sizeOfHall - sizeOfWardrobe - sizeOfBench;
-            double countOfDancers = freeSpace / (40+7000);
+            double countOfDancers = freeSpace / (40 + 7000);
+            
             Console.WriteLine(Math.Floor(countOfDancers));
         }
     }
