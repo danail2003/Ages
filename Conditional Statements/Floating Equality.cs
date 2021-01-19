42c1217b8bef214755e0c9ff12bc84e492a2b9fc
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-19 19:58:15 +0200

    Update and rename Floating Equality to Conditional Statements/Floating Equality.cs

--- Conditional Statements/Floating Equality.cs
+++ Conditional Statements/Floating Equality.cs
@@ -4,10 +4,11 @@ namespace Floating_Equality
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double firstNumber = double.Parse(Console.ReadLine());
             double secondNumber = double.Parse(Console.ReadLine());
+            
             double eps = 0.000001;
 
             if (Math.Abs(firstNumber - secondNumber) <= eps)
