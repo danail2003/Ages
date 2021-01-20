78613dec36d67789909d44a0c94dd21cf290c98b
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-20 20:08:15 +0200

    Update and rename FromInchToCentimetres to Simple Operations/FromInchToCentimetres.cs

--- Simple Operations/FromInchToCentimetres.cs
+++ Simple Operations/FromInchToCentimetres.cs
@@ -4,10 +4,12 @@ namespace FromInchToCentimetres
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double inches = double.Parse(Console.ReadLine());
+            
             double centimeters = inches * 2.54;
+            
             Console.WriteLine("{0:f2}",centimeters);            
         }
     }
