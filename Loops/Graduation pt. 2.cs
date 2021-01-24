a785b4d440c0109e7a7d5ae7cfb3c0a72d0bdd55
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-24 20:30:06 +0200

    Update and rename Graduation pt. 2 to Loops/Graduation pt. 2.cs

--- Loops/Graduation pt. 2.cs
+++ Loops/Graduation pt. 2.cs
@@ -4,28 +4,26 @@ namespace Graduation_pt._2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string name = Console.ReadLine();
 
             int counter = 0;
             int counter2 = 0;
-            
             double total = 0.0;
 
             while (counter < 12)
             {
                 double grade = double.Parse(Console.ReadLine());
+                
                 if (grade < 4)
                 {
                     counter2++;
-                    
                     if (counter2 == 2)
                     {                     
                         Console.WriteLine($"{name} has been excluded at {counter} grade");
                         break;
-                    }
-                    
+                    } 
                 }
                 else
                 {
@@ -41,4 +39,3 @@ namespace Graduation_pt._2
         }
     }
 }
-
