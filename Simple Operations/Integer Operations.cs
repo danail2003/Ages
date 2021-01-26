7e1389e8b6ee416e9eaa708781e9c7b75d12d24f
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-26 18:55:51 +0200

    Update and rename Integer Operations to Simple Operations/Integer Operations.cs

--- Simple Operations/Integer Operations.cs
+++ Simple Operations/Integer Operations.cs
@@ -4,13 +4,15 @@ namespace Integer_Operations
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int firstNumber = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
             int thirdNumber = int.Parse(Console.ReadLine());
             int fourthNumber = int.Parse(Console.ReadLine());
+            
             double operation = (firstNumber + secondNumber) / thirdNumber * fourthNumber;
+            
             Console.WriteLine(operation);
         }
     }
