74f832e58e55c3034bf4b8b23ec046e1b473c501
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-01-27 19:50:02 +0200

    Update and rename Lower or Upper to Conditional Statements/Lower or Upper.cs

--- Conditional Statements/Lower or Upper.cs
+++ Conditional Statements/Lower or Upper.cs
@@ -4,11 +4,11 @@ namespace Lower_or_Upper
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             char symbol = char.Parse(Console.ReadLine());
 
-            if(symbol >= 65 && symbol <= 90)
+            if (symbol >= 65 && symbol <= 90)
             {
                 Console.WriteLine("upper-case");
             }
