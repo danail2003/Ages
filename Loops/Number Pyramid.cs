178ad79b60246f200276bdc8cd1ed849687d59a1
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-01 18:02:16 +0200

    Update and rename Number Pyramid to Loops/Number Pyramid.cs

--- Loops/Number Pyramid.cs
+++ Loops/Number Pyramid.cs
@@ -7,10 +7,11 @@ namespace Number_Pyramid
         static void Main()
         {
             int number = int.Parse(Console.ReadLine());
+            
             int currentNum = 1;
             bool isBigger = false;
 
-            for(int rows = 1; rows <= number; rows++)
+            for (int rows = 1; rows <= number; rows++)
             {
                 for (int columns = 1; columns <= rows; columns++)
                 {
