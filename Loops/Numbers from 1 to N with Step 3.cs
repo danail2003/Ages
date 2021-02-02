7788925b0a92c524d9cb3eddd3a07fde623f21b7
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-02 19:36:33 +0200

    Update and rename Numbers from 1 to N with Step 3 to Loops/Numbers from 1 to N with Step 3.cs

--- Loops/Numbers from 1 to N with Step 3.cs
+++ Loops/Numbers from 1 to N with Step 3.cs
@@ -8,7 +8,7 @@ namespace Numbers_from_1_to_N_with_Step_3
         {
             int number = int.Parse(Console.ReadLine());
 
-            for(int i = 1; i <= number; i += 3)
+            for (int i = 1; i <= number; i += 3)
             {
                 Console.WriteLine(i);
             }
