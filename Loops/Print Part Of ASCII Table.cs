0ee3b013db218951390755b0d3a70b0000c87b19
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-08 17:20:38 +0200

    Update and rename Print Part Of ASCII Table to Loops/Print Part Of ASCII Table.cs

--- Loops/Print Part Of ASCII Table.cs
+++ Loops/Print Part Of ASCII Table.cs
@@ -4,14 +4,14 @@ namespace Print_Part_Of_ASCII_Table
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int firstNumber = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
 
             for (char i = (char)firstNumber; i <= secondNumber; i++)
             {
-                Console.Write(i+" ");
+                Console.Write(i + " ");
             }
         }
     }
