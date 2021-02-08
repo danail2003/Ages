6c90967f9ab23c94802ca096a0cc1fb3d551a179
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-08 17:18:41 +0200

    Update and rename Pounds to Dollars to Simple Operations/Pounds to Dollars.cs

--- Simple Operations/Pounds to Dollars.cs
+++ Simple Operations/Pounds to Dollars.cs
@@ -4,10 +4,12 @@ namespace Pounds_to_Dollars
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double pounds = double.Parse(Console.ReadLine());
+            
             double dollars = pounds * 1.31;
+            
             Console.WriteLine($"{dollars:f3}");
         }
     }
