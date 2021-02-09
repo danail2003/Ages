cd9ebd19a0d15dfe607152d4fc9a0f907aa84a1b
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-09 18:52:43 +0200

    Update and rename Radians to Degrees to Simple Operations/Radians to Degrees.cs

--- Simple Operations/Radians to Degrees.cs
+++ Simple Operations/Radians to Degrees.cs
@@ -4,10 +4,12 @@ namespace Radians_to_Degrees
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double rad = double.Parse(Console.ReadLine());
+            
             double degree = rad * 180 / Math.PI;
+            
             Console.WriteLine(Math.Round(degree));
         }
     }
