d9e03eef42f93606513fb5eafe1e639e0a18692d
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-09 20:51:42 +0200

    Update and rename Refactor Volume of Pyramid to Simple Operations/Refactor Volume of Pyramid.cs

--- Simple Operations/Refactor Volume of Pyramid.cs
+++ Simple Operations/Refactor Volume of Pyramid.cs
@@ -4,12 +4,14 @@ namespace Refactor_Volume_of_Pyramid
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double length = double.Parse(Console.ReadLine());
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+            
             double volume = 1 / 3.0 * length * width * height;
+            
             Console.WriteLine($"Length: Width: Height: Pyramid Volume: {volume:f2}");      
         }
     }
