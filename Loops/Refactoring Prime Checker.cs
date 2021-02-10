1538fedd074cfb6eb28f2defceeaae6a26333da9
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-10 20:41:42 +0200

    Update and rename Refactoring Prime Checker to Loops/Refactoring Prime Checker.cs

--- Loops/Refactoring Prime Checker.cs
+++ Loops/Refactoring Prime Checker.cs
@@ -4,9 +4,10 @@ namespace Refactoring_Prime_Checker
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int number = int.Parse(Console.ReadLine());
+            
             for (int i = 2; i <= number; i++)
             {
                 bool isPrime = true;
@@ -21,7 +22,8 @@ namespace Refactoring_Prime_Checker
                         break;
                     }
                 }
-                Console.WriteLine("{0} -> {1}", i, check);
+                Con
+                sole.WriteLine("{0} -> {1}", i, check);
             }
         }
     }   
