63abcf13709571206138d760ffee5b382886e49b
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-13 18:48:45 +0200

    Update and rename Sort Numbers to Conditional Statements/Sort Numbers.cs

--- Conditional Statements/Sort Numbers.cs
+++ Conditional Statements/Sort Numbers.cs
@@ -4,43 +4,43 @@ namespace Sort_Numbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             int firsNumber = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
             int thirdNumber = int.Parse(Console.ReadLine());
 
-            if(firsNumber>secondNumber && firsNumber>thirdNumber && secondNumber >=thirdNumber)
+            if (firsNumber > secondNumber && firsNumber > thirdNumber && secondNumber >= thirdNumber)
             {
                 Console.WriteLine(firsNumber);
                 Console.WriteLine(secondNumber);
                 Console.WriteLine(thirdNumber);
             }
-            else if(firsNumber>secondNumber && firsNumber>thirdNumber && thirdNumber >=secondNumber)
+            else if (firsNumber > secondNumber && firsNumber > thirdNumber && thirdNumber >= secondNumber)
             {
                 Console.WriteLine(firsNumber);
                 Console.WriteLine(thirdNumber);
                 Console.WriteLine(secondNumber);
             }
-            else if(secondNumber>firsNumber && secondNumber>thirdNumber && firsNumber >=thirdNumber)
+            else if (secondNumber > firsNumber && secondNumber > thirdNumber && firsNumber >= thirdNumber)
             {
                 Console.WriteLine(secondNumber);
                 Console.WriteLine(firsNumber);
                 Console.WriteLine(thirdNumber);
             }
-            else if(secondNumber>firsNumber && secondNumber>thirdNumber && thirdNumber >=firsNumber)
+            else if (secondNumber > firsNumber && secondNumber > thirdNumber && thirdNumber >= firsNumber)
             {
                 Console.WriteLine(secondNumber);
                 Console.WriteLine(thirdNumber);
                 Console.WriteLine(firsNumber);
             }
-            else if(thirdNumber>firsNumber && thirdNumber>secondNumber && firsNumber >=secondNumber)
+            else if (thirdNumber > firsNumber && thirdNumber > secondNumber && firsNumber >= secondNumber)
             {
                 Console.WriteLine(thirdNumber);
                 Console.WriteLine(firsNumber);
                 Console.WriteLine(secondNumber);
             }
-            else if(thirdNumber>firsNumber && thirdNumber>secondNumber && secondNumber >=firsNumber)
+            else if (thirdNumber > firsNumber && thirdNumber > secondNumber && secondNumber >= firsNumber)
             {
                 Console.WriteLine(thirdNumber);
                 Console.WriteLine(secondNumber);
