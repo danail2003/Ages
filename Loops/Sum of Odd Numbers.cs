6b02d51a5499ad2f990541af54f74c470d273462
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-16 21:49:31 +0200

    Update and rename Sum of Odd Numbers to Loops/Sum of Odd Numbers.cs

--- Loops/Sum of Odd Numbers.cs
+++ Loops/Sum of Odd Numbers.cs
@@ -4,9 +4,10 @@ namespace Sum_of_Odd_Numbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int number = int.Parse(Console.ReadLine());           
+            int number = int.Parse(Console.ReadLine());
+            
             int odd = 0;
             int sum = 0;
 
@@ -15,10 +16,12 @@ namespace Sum_of_Odd_Numbers
                 if (i % 2 != 0)
                 {
                     Console.WriteLine(i);
+                    
                     odd++;
                     sum += i;
                 }
             }
+            
             Console.WriteLine($"Sum: {sum}");
         }
     }
