0c41e30104ce83a76176b738b607e675c8dfcba8
Danail Dobrev <56361040+danail2003@users.noreply.github.com>
2021-02-20 09:14:19 +0200

    Update and rename Yard Greening to Simple Operations/Yard Greening.cs

--- Simple Operations/Yard Greening.cs
+++ Simple Operations/Yard Greening.cs
@@ -4,12 +4,14 @@ namespace Yard_Greening
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double yard = double.Parse(Console.ReadLine());
+            
             double price = yard * 7.61;
             double discount = 0.18 * price;
             double end = price - discount;
+            
             Console.Write("The final price is: ");
             Console.WriteLine($"{end:f2} lv.");
             Console.Write("The discount is: ");
